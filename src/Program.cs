36e00dd9c42b95c9bab5e1d3a615dd0f6ab090eb
Trace <lukaszdabrowski881@gmail.com>
2021-12-20 00:53:25 +0100

    set up Remora DI

--- src/Program.cs
+++ src/Program.cs
@@ -2,18 +2,23 @@
 using Microsoft.Extensions.DependencyInjection;
 using Mmcc.CrashAlert;
 using Mmcc.CrashAlert.Commands;
+using Mmcc.CrashAlert.Converters;
 using Mmcc.CrashAlert.Database;
 using Mmcc.CrashAlert.Services;
+using Remora.Discord.Rest.Extensions;
 
 var services = new ServiceCollection();
 
 services.AddDbContext<CrashAlertContext>();
+services.AddDiscordRest(_ => "crashalert");
 
 services.AddScoped<CommandContext>();
 services.AddScoped<ProcessedCrashLogsService>();
+services.AddScoped<CrashNotificationService>();
 services.AddScoped<CrashLogsProcessingService>();
 
 // commands;
+services.AddTransient<SnowflakeConverter>();
 services.AddTransient<ProcessExitCommand>();
 
 var sp = services.BuildServiceProvider();
