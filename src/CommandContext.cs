6676d4804f742e4d5e4f1f9d2783b043b2616f6f
Trace <lukaszdabrowski881@gmail.com>
2021-12-20 00:52:41 +0100

    add webhook settings to command context

--- src/CommandContext.cs
+++ src/CommandContext.cs
@@ -1,10 +1,13 @@
+using Remora.Rest.Core;
+
 namespace Mmcc.CrashAlert;
 
 public class CommandContext
 {
     public string BaseDir { get; set; } = null!;
-    
     public int Interval { get; set; }
+    public string DiscordWebhookToken { get; set; } = null!;
+    public Snowflake DiscordWebhookId { get; set; }
     public string ServerRuntimeDir => Path.Join(BaseDir, "server-runtime");
     public string CrashLogsDir => Path.Join(ServerRuntimeDir, "crash-reports");
 }
